567a22f80a2561b261d12aa287876d77bbbbd31c
David Dongmin Oh <doh24@wisc.edu>
2021-10-12 20:32:12 -0500

    upload: price inquiry on pyQt

---
+++ PyQt+pybithumb/priceInquiry.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+import pybithumb
+
+form_class = uic.loadUiType(
+    "window.ui")[0]
+
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.inquiry)
+        self.timer = QTimer(self)       # create QTimer instance
+        self.timer.start(1000)          # set inverval as 1 sec
+        self.timer.timeout.connect(self.inquiry)    # even loop in every second
+
+    def inquiry(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
+        price = pybithumb.get_current_price("BTC")
+        self.lineEdit.setText(str(price))
+
+
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec_()
