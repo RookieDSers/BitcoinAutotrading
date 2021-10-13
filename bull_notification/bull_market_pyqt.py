ce9e17de30aab97e9febdd6506acbe1089217d30
David Dongmin Oh <doh24@wisc.edu>
2021-10-12 20:46:38 -0500

    upload: bull notification with pyqt

---
+++ bull_notification/bull_market_pyqt.py
@@ -0,0 +1,54 @@
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+import pybithumb
+
+form_class = uic.loadUiType(
+    "/Volumes/SteveJobs/localGit/bitcoin_tutorial/5_BullNotification/bull.ui")[0]
+tickers = ["BTC", "ETH", "BCH", "ETC"]
+
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.tableWidget.setRowCount(len(tickers))  # set rowNum to tickers
+        for i, ticker in enumerate(tickers):
+            item = QTableWidgetItem(ticker)
+            self.tableWidget.setItem(i, 0, item)
+
+        timer = QTimer(self)
+        timer.start(5000)
+        timer.timeout.connect(self.timeout)
+
+    def get_market_infos(self, ticker):
+        df = pybithumb.get_ohlcv(ticker)
+        ma5 = df['close'].rolling(5).mean()
+        last_ma5 = ma5[-2]
+        price = pybithumb.get_current_price(ticker)
+
+        state = None
+        if price > last_ma5:
+            state = "Bull!!!"
+        else:
+            state = "Bear..."
+
+        return price, last_ma5, state
+
+    def timeout(self):
+        for i, ticker in enumerate(tickers):
+            # item = QTableWidgetItem(ticker)
+            # self.tableWidget.setItem(i, 0, item)
+
+            price, last_ma5, state = self.get_market_infos(ticker)
+            self.tableWidget.setItem(i, 1, QTableWidgetItem(str(price)))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(str(last_ma5)))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(state))
+
+
+app = QApplication(sys.argv)
+win = MyWindow()
+win.show()
+app.exec_()
