d6edc648d72f9d04a4624b80db9e3811276c4928
davidoh919 <doh24@wisc.edu>
2021-11-13 23:13:50 -0600

    edit ma5_strategy code

--- bull_notification/ma5_strategy.py
+++ bull_notification/ma5_strategy.py
@@ -10,7 +10,6 @@ print(close)
 # Calculate "Moving Average"
 window = close.rolling(5)   # group the data with size 5
 ma5 = window.mean()
-# ma5 = close.rolling(5).mean()
 print(ma5)
 
 # Bull notification
