ee01346b5a9866c8bf71440136c41c9c78fa6f54
hoonkiyeo <hky1721@gmail.com>
2021-11-27 19:41:51 -0600

    create HTS folder and update volatility.py

---
+++ HTS/volatility.py
@@ -0,0 +1,100 @@
+import datetime
+import time
+import pybithumb
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+class VolatilityWorker(QThread):
+    tradingSent = pyqtSignal(str, str, str)
+
+    def __init__(self, ticker, bithumb):
+        super().__init__()
+        self.ticker = ticker
+        self.bithumb = bithumb
+        self.alive = True
+
+    def run(self):
+        now = datetime.datetime.now()
+        mid = datetime.datetime(now.year, now.month,
+                                now.day) + datetime.timedelta(1)
+        ma5 = get_yesterday_ma5(self.ticker)
+        target_price = get_target_price(self.ticker)
+        wait_flag = False
+
+        print(target_price)
+        print(ma5)
+
+        while self.alive:
+            try:
+                now = datetime.datetime.now()
+                if mid < now < mid + datetime.delta(seconds=10):
+                    print("Try to sell...")
+                    target_price = get_target_price(self.ticker)
+                    mid = datetime.datetime(
+                        now.year, now.month, now.day) + datetime.timedelta(1)
+                    ma5 = get_yesterday_ma5(self.ticker)
+                    desc = sell_crypto_currency(self.bithumb, self.ticker)
+
+                    result = self.bithumb.get_order_completed(desc)
+                    timestamp = result['data']['order_date']
+                    dt = datetime.datetime.fromtimestamp(
+                        int(int(timestamp)/1000000))
+                    tstring = dt.strftime("%Y/%m/%d %H:%M:%S")
+                    self.tradingSent.emit(
+                        tstring, "Sell", result['data']['order_qty'])
+                    wait_flag = False
+                    print("Sold!!")
+
+                if wait_flag == False:
+                    # print("try to buy...")
+                    current_price = pybithumb.get_current_price(self.ticker)
+                    # print(current_price, "<--->", target_price, "with", ma5)
+                    if (current_price > target_price) and (current_price > ma5):
+                        print("time to buy...!")
+                        desc = buy_crypto_currency(self.bithumb, self.ticker)
+                        result = self.bithumb.get_order_completed(desc)
+                        timestamp = result['data']['order_date']
+                        dt = datetime.datetime.fromtimestamp(
+                            int(int(timestamp)/1000000))
+                        tstring = dt.strftime("%Y/%m/%d %H:%M:%S")
+                        self.tradingSent.emit(
+                            tstring, "Buy", result['data']['order_qty'])
+                        wait_flag = True
+                        print(tstring, "bought!!")
+            except:
+                pass
+            time.sleep(1)
+
+    def close(self):
+        self.alive = False
+
+
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    yesterday = df.iloc[-2]
+
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
+
+
+def buy_crypto_currency(bithumb, ticker):
+    krw = bithumb.get_balance(ticker)[2]
+    orderbook = pybithumb.get_orderbook(ticker)
+    sell_price = orderbook['asks'][0]['price']
+    unit = krw/float(sell_price) * 0.7
+    return bithumb.buy_market_order(ticker, unit)
+
+
+def sell_crypto_currency(bithumb, ticker):
+    unit = bithumb.get_balance(ticker)[0]
+    return bithumb.sell_market_order(ticker, unit)
+
+
+def get_yesterday_ma5(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    close = df['close']
+    ma = close.rolling(5).mean()
+    return ma[-2]
